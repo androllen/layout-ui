2c9e012d020d7d17ba2d107e245cf1c95fa15591
androllen <androllen@hotmail.com>
2020-10-26 16:05:30 +0800

    update layout

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,10 @@
 <template>
-  <el-container :style="{ height: winHeight + 'px' }">
-    <el-aside :style="{ height: winHeight + 'px' }">
-      <div class="block">
+  <el-container id="shell">
+    <div class="el-nav" id="shell">
+      <div class="logo">
         <el-input v-model="input" placeholder="请输入内容"></el-input>
       </div>
-      <div class="scrolldiv nui-scroll">
+      <div class="views nui-scroll">
         <div id="sample">
           <p>Sample flexbox example</p>
         </div>
@@ -101,12 +101,138 @@
           </div>
         </div>
       </div>
-      <div class="bottom"></div>
-    </el-aside>
- 
-    <div class="contentpanel" :style="{ height: winHeight + 'px' }">
+      <div class="setting"></div>
+    </div>
+
+    <div class="el-content" id="shell">
       <div class="title">11</div>
-      <div class="views">22</div>
+      <div class="views nui-scroll">
+        <div>
+          <p>Sample flexbox example</p>
+        </div>
+        <div id="itemlist">
+          <div id="item">
+            <div>
+              <p>First article</p>
+              <article>
+                So, this single declaration gives us everything we need — incredible, right? We have our multiple column layout
+                with equal-sized columns, and the columns are all the same height. This is because the default values given to
+                flex items (the children of the flex container) are set up to solve common problems such as this.
+              </article>
+            </div>
+          </div>
+          <div id="item">
+            <div>
+              <p>Second article</p>
+              <article>
+                So, this single declaration gives us everything we need — incredible, right? We have our multiple column layout
+                with equal-sized columns, and the columns are all the same height. This is because the default values given to
+                flex items (the children of the flex container) are set up to solve common problems such as this.
+              </article>
+            </div>
+          </div>
+          <div id="item">
+            <div>
+              <p>Third article</p>
+              <article>
+                So, this single declaration gives us everything we need — incredible, right? We have our multiple column layout
+                with equal-sized columns, and the columns are all the same height. This is because the default values given to
+                flex items (the children of the flex container) are set up to solve common problems such as this.
+              </article>
+            </div>
+          </div>
+          <div id="item">
+            <div>
+              <p>First article</p>
+              <article>
+                So, this single declaration gives us everything we need — incredible, right? We have our multiple column layout
+                with equal-sized columns, and the columns are all the same height. This is because the default values given to
+                flex items (the children of the flex container) are set up to solve common problems such as this.
+              </article>
+            </div>
+          </div>
+          <div id="item">
+            <div>
+              <p>Second article</p>
+              <article>
+                So, this single declaration gives us everything we need — incredible, right? We have our multiple column layout
+                with equal-sized columns, and the columns are all the same height. This is because the default values given to
+                flex items (the children of the flex container) are set up to solve common problems such as this.
+              </article>
+            </div>
+          </div>
+          <div id="item">
+            <div>
+              <p>Third article</p>
+              <article>
+                So, this single declaration gives us everything we need — incredible, right? We have our multiple column layout
+                with equal-sized columns, and the columns are all the same height. This is because the default values given to
+                flex items (the children of the flex container) are set up to solve common problems such as this.
+              </article>
+            </div>
+          </div>
+          <div id="item">
+            <div>
+              <p>First article</p>
+              <article>
+                So, this single declaration gives us everything we need — incredible, right? We have our multiple column layout
+                with equal-sized columns, and the columns are all the same height. This is because the default values given to
+                flex items (the children of the flex container) are set up to solve common problems such as this.
+              </article>
+            </div>
+          </div>
+          <div id="item">
+            <div>
+              <p>Second article</p>
+              <article>
+                So, this single declaration gives us everything we need — incredible, right? We have our multiple column layout
+                with equal-sized columns, and the columns are all the same height. This is because the default values given to
+                flex items (the children of the flex container) are set up to solve common problems such as this.
+              </article>
+            </div>
+          </div>
+          <div id="item">
+            <div>
+              <p>Third article</p>
+              <article>
+                So, this single declaration gives us everything we need — incredible, right? We have our multiple column layout
+                with equal-sized columns, and the columns are all the same height. This is because the default values given to
+                flex items (the children of the flex container) are set up to solve common problems such as this.
+              </article>
+            </div>
+          </div>
+          <div id="item">
+            <div>
+              <p>First article</p>
+              <article>
+                So, this single declaration gives us everything we need — incredible, right? We have our multiple column layout
+                with equal-sized columns, and the columns are all the same height. This is because the default values given to
+                flex items (the children of the flex container) are set up to solve common problems such as this.
+              </article>
+            </div>
+          </div>
+          <div id="item">
+            <div>
+              <p>Second article</p>
+              <article>
+                So, this single declaration gives us everything we need — incredible, right? We have our multiple column layout
+                with equal-sized columns, and the columns are all the same height. This is because the default values given to
+                flex items (the children of the flex container) are set up to solve common problems such as this.
+              </article>
+            </div>
+          </div>
+          <div id="item">
+            <div>
+              <p>Third article</p>
+              <article>
+                So, this single declaration gives us everything we need — incredible, right? We have our multiple column layout
+                with equal-sized columns, and the columns are all the same height. This is because the default values given to
+                flex items (the children of the flex container) are set up to solve common problems such as this.
+              </article>
+            </div>
+          </div>
+        </div>
+      </div>
       <div class="logger">33</div>
     </div>
   </el-container>
@@ -116,6 +242,22 @@
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue';
 
+/*
+ DIV高度为浏览器窗口的高度
+ 通过深入Document内部对body进行检测，获取浏览器窗口高度
+*/
+function autodivheight() {
+  let winHeight = 0;
+  if (window.innerHeight) {
+    winHeight = window.innerHeight;
+  } else if (document.body && document.body.clientHeight) {
+    winHeight = document.body.clientHeight;
+  }
+  if (document.documentElement && document.documentElement.clientHeight) {
+    winHeight = document.documentElement.clientHeight;
+  }
+  document.getElementById('shell').style.height = winHeight + 'px';
+}
 export default {
   name: 'Home',
   components: {
@@ -123,20 +265,22 @@ export default {
   },
   data() {
     return {
-      winHeight: document.documentElement.clientHeight,
       input: '',
       src: 'https://cube.elemecdn.com/6/94/4d3ea53c084bad6931a56d5158a48jpeg.jpeg',
     };
   },
   mounted() {
+    window.onload = () => {
+      autodivheight();
+    };
     window.onresize = () => {
-      this.winHeight = document.documentElement.clientHeight;
+      autodivheight();
     };
   },
 };
 </script>
 
-<style >
+<style>
 .title {
   background-color: #333;
   height: 60px;
@@ -147,19 +291,17 @@ export default {
 }
 
 .views {
-  background-color: #8eb1e8;
+  background-color: transparent;
   flex: 1;
+  height: 100%;
 }
 
-.contentpanel {
+.el-content {
   display: flex;
   flex-direction: column;
   width: 100%;
 }
-.scrolldiv {
-  height: 100%;
-  overflow: hidden;
-}
+
 .el-aside {
   background-color: #d3dce6;
   color: #333;
@@ -167,17 +309,24 @@ export default {
   display: flex;
   flex-direction: column;
 }
-.block {
+.el-nav {
+  display: flex;
+  flex-direction: column;
+  width: 250px;
+}
+.logo {
   background: #1f3359;
-  z-index: 99;
+  height: 60px;
+}
+.setting {
+  background-color: #8eb1e8;
+  height: 100px;
 }
 
 #item {
   background-color: turquoise;
   padding: 0 10px 10px 10px;
-  /* 强制设置最小宽度 */
   flex: 200px;
-  /* flex-basis: 200px; */
 }
 #itemlist {
   display: flex;
@@ -191,10 +340,6 @@ export default {
   position: fixed;
   left: 0;
   top: 0;
-}
-.bottom {
-  background-color: #8eb1e8;
-  height: 100px;
 }
 
 .nui-scroll {
@@ -211,7 +356,7 @@ export default {
 /*正常情况下滑块的样式*/
 .nui-scroll::-webkit-scrollbar-thumb {
   border-radius: 4px;
-  background: #8eb1e8;
+  background: #e8b28e;
 }
 
 /*正常时候的主干部分*/
